6632c9f009461e0eaa9ab8cbe63ba2f237203422
MrK <crodriguezq91@gmail.com>
2019-01-23 20:08:00 -0600

    Starting Fashion Site Web Project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="container">
+    <mainMenu>
+      <btn btnColor="btn btn-small btn-info btn-popup"
+         :cartIcon="true"
+         @click.native="showPopupCart()">
+         Cart
+        <span class="btn-circle" v-if="hasProduct()">
+           {{ getProductsInCart.length }}
+        </span>
+      </btn>
+      <transition name="appear">
+        <popupcart class="cart" v-if="getPopupCart"/>
+      </transition>
+    </mainMenu>
+    <transition name="leave">
+      <router-view></router-view>
+    </transition>
+    <maskBg v-if="getPopupCart" @click.native="showPopupCart()"/>
   </div>
 </template>
 
---
+++ src/components/Menu.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <ul class="nav-links">
+        <li class="link">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="link">
+          <router-link to="/about">About</router-link>
+        </li>
+        <li class="link">
+          <router-link to="/clothes">Clothes</router-link>
+        </li>
+        <li class="link">
+          <router-link to="/selected">Selected</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .header {
+    width: 100%;
+    height: 70px;
+    background-color: #333333;
+    box-sizing: border-box;
+    padding: .5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .nav {
+    width: 600px;
+  }
+  .nav-links {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+  }
+  .link {
+    list-style: none;
+    padding: 0 2em;
+  }
+  .link a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
