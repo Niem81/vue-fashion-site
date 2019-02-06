c878f7de81cb72f662a1dac1c42696120957239e
MrK <crodriguezq91@gmail.com>
2019-02-05 19:06:34 -0600

    Updating Clothes View

--- src/views/Clothes.vue
+++ src/views/Clothes.vue
@@ -1,5 +1,15 @@
 <template lang="html">
-
+  <div class="clothes-sample">
+    <ul class="clothes-list">
+      <transition-group name="fade">
+        <li v-for="(product, index) in getProductsInCart" :key="index" class="checkout-product">
+           <img :src="product.image" alt="" class="product-image">
+           <h3 class="product-name">{{ product.name }}</h3>
+           <span class="product-price">R$ {{ product.price }},00 </span>
+         </li>
+      </transition-group>
+    </ul>
+  </div>
 </template>
 
 <script>
